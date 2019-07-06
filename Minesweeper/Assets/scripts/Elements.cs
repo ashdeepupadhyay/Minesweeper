84c750ed7bcd3568bc15095c8f02940ff20e5606
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2019-07-06 16:59:43 +0530

    basic functionality

---
+++ Minesweeper/Assets/scripts/Elements.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elements : MonoBehaviour {
+    public bool mine;
+    public Sprite[] emptyTextures;
+    public Sprite mineTexture;
+	// Use this for initialization
+	void Start () {
+        mine = Random.value < 0.15;
+        int x = (int)transform.position.x;
+        int y = (int)transform.position.y;
+        Grid.elements[x, y] = this;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadTextures(int adjacentCount){
+        if (mine){
+            GetComponent<SpriteRenderer>().sprite = mineTexture;
+        }
+        else{
+            GetComponent<SpriteRenderer>().sprite = emptyTextures[adjacentCount];
+        }
+    }
+
+    public bool IsCovered()
+    {
+        return GetComponent<SpriteRenderer>().sprite.texture.name == "unexplored";
+    }
+
+    public void OnMouseUpAsButton(){
+        if (mine){
+            Grid.UncoveredMines();
+            print("YOU LOSE");
+        }
+        else{
+            Debug.Log("i am clicked");
+            int x = (int)transform.position.x;
+            int y = (int)transform.position.y;
+            LoadTextures(Grid.AdjacentMines(x, y));
+            
+        }
+    }
+}
---
+++ Minesweeper/Assets/scripts/Grid.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    public static int w = 10;
+    public static int h = 13;
+    public static Elements[,] elements = new Elements[w, h];
+
+    public static void UncoveredMines(){
+        foreach(Elements elem in elements){
+            if (elem.mine){
+                elem.LoadTextures(0);
+            }
+        }
+    }
+
+    public static bool MineAt(int x,int y){
+        if (x >= 0 && y >= 0 && x < w && y < w){
+            return elements[x, y].mine;
+        }
+        return false;
+    }
+
+    public static int AdjacentMines(int x,int y){
+        int count = 0;
+
+        if (MineAt(x, y + 1)){//top
+            ++count;
+        }
+        if (MineAt(x+1, y + 1)){//topright
+            ++count;
+        }
+        if (MineAt(x+1, y)){//right
+            ++count;
+        }
+        if (MineAt(x+1, y - 1)){//bottomRight
+            ++count;
+        }
+        if (MineAt(x, y - 1)){//bottom
+            ++count;
+        }
+        if (MineAt(x-1, y - 1)){//bottomleft
+            ++count;
+        }
+        if (MineAt(x-1, y)){//left
+            ++count;
+        }
+        if (MineAt(x-1, y + 1)){//topleft
+            ++count;
+        }
+        return count;
+    }
+}
